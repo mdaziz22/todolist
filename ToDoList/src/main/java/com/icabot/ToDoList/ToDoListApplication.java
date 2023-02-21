02d270f1ea0162afbd6d65c044321be405fa61ae
ryan <practiceprogramming6@gmail.com>
2023-02-21 20:51:20 +0530

    added getUsers API for populating DB

--- ToDoList/src/main/java/com/icabot/ToDoList/ToDoListApplication.java
+++ ToDoList/src/main/java/com/icabot/ToDoList/ToDoListApplication.java
@@ -1,12 +1,18 @@
 package com.icabot.ToDoList;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class ToDoListApplication {
+@RestController
+public class ToDoListApplication  {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
--- ToDoList/src/main/java/com/icabot/ToDoList/controller/UserController.java
+++ ToDoList/src/main/java/com/icabot/ToDoList/controller/UserController.java
@@ -2,17 +2,38 @@ package com.icabot.ToDoList.controller;
 
 import com.icabot.ToDoList.dto.UserDto;
 import com.icabot.ToDoList.model.User;
+import com.icabot.ToDoList.repository.UserRepository;
 import com.icabot.ToDoList.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
-
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/getUsers")
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> list = Arrays.asList(
+                User.builder().userName("userName1")
+                        .name("name1")
+                        .password("pass1")
+                        .build(),
+                User.builder().userName("userName2")
+                        .name("name2")
+                        .password("pass12")
+                        .build()
+        );
+        List<User> listFromDb = userRepository.saveAll(list);
+        return new  ResponseEntity<List<User>>(listFromDb, HttpStatus.OK);
+    }
     @PostMapping("/createUser")
     public ResponseEntity<User> createUser(@RequestBody UserDto userDto){
         User userFromDB = userService.createUser(userDto);
